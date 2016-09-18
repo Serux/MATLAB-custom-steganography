4a3f051a5ed38d2ba35949f9c780186d65452ed7
Sergio Chico <serchi93@hotmail.com>
2016-09-19 01:17:59 +0200

    Rename Final.m to Matlab_Steganography.m

--- Matlab_Steganography.m
+++ Matlab_Steganography.m
@@ -1,25 +1,25 @@
-%% Definición de características.
+%% DefiniciÃ³n de caracterÃ­sticas.
 
 %Cabecera de mensaje cifrado:
-%(Si se modifican estas características, el programa debería seguir
-%funcionando, ya que todos los cálculos con tamaños de la cabecera se 
+%(Si se modifican estas caracterÃ­sticas, el programa deberÃ­a seguir
+%funcionando, ya que todos los cÃ¡lculos con tamaÃ±os de la cabecera se 
 %han realizado usando estas variables)
 
 %1 bit tipo de datos (texto-imagen)
 BitsTipoDatos = 1;
-%11 bits por altitud (máximo 2047)
+%11 bits por altitud (mÃ¡ximo 2047)
 BitsAltitud = 11;
-%11 bits por longitud (máximo 2047)
+%11 bits por longitud (mÃ¡ximo 2047)
 BitsLongitud = 11;
 %1 bit por blanco y negro o rgb.
 BitsColor = 1;
 %21 bits por longitud de texto,
 BitsStringLength = 23;
 
-%La tamaño máximo de la imagen contenida o del texto no podrá ser mayor 
-%que estos valores, pero el tamaño máximo final se verá limitado por el
-%tamaño de la imagen contenedora (por cada byte se puede cifrar un bit,
-%por lo tanto se puede cifrar un archivo de tamaño 1/8)
+%La tamaÃ±o mÃ¡ximo de la imagen contenida o del texto no podrÃ¡ ser mayor 
+%que estos valores, pero el tamaÃ±o mÃ¡ximo final se verÃ¡ limitado por el
+%tamaÃ±o de la imagen contenedora (por cada byte se puede cifrar un bit,
+%por lo tanto se puede cifrar un archivo de tamaÃ±o 1/8)
 
 BytesTotalesCabeceraImagen= (BitsTipoDatos + BitsAltitud + BitsLongitud + BitsColor)/8;
 BytesTotalesCabeceraTexto = (BitsTipoDatos + BitsStringLength)/8;
@@ -33,7 +33,7 @@ while opcionMenu ~= 0
             tipomensajequeseintroducira = input('Introduce 0 para cifrar texto o 1 para cifrar imagen: ');
         end
         
-        %% Obtener imagen sobre la que se introducirá mensaje oculto.
+        %% Obtener imagen sobre la que se introducirÃ¡ mensaje oculto.
         continuar = false;
         disp('Selecciona una imagen sobre la que introducir el mensaje');
         while  ~continuar;
@@ -50,29 +50,29 @@ while opcionMenu ~= 0
         sizeZ=size(A,3);
         
         
-        %% Convierte imagen en representación binaria.
-        disp('Convirtiendo a representación binaria');
+        %% Convierte imagen en representaciÃ³n binaria.
+        disp('Convirtiendo a representaciÃ³n binaria');
         Abin=dec2bin(A,8);
         ABinVaciada = Abin;
         
-        %% Vacía último bit todos los píxeles de la imagen.
-        disp('Vaciando último bit de toda la imagen');
+        %% VacÃ­a Ãºltimo bit todos los pÃ­xeles de la imagen.
+        disp('Vaciando Ãºltimo bit de toda la imagen');
         ABinVaciada(:,8) = '0';
         
-        %% Convertir Representación binaria en imagen pasa mostrarla.
-        disp('Convirtiendo a imagen Vacía');
+        %% Convertir RepresentaciÃ³n binaria en imagen pasa mostrarla.
+        disp('Convirtiendo a imagen VacÃ­a');
         AVaciada=uint8(bin2dec(ABinVaciada));
         AVaciada=reshape(AVaciada,sizeX,sizeY,sizeZ);
         
         
         %% Procesado de mensaje para introducirlo en la imagen
         if tipomensajequeseintroducira == 0
-            %% Introducción mensaje de texto.
+            %% IntroducciÃ³n mensaje de texto.
             maxTextSize= size(Abin,1)/8;
-            texto = input(['Introduce mensaje a cifrar, máximo (', num2str(maxTextSize - BytesTotalesCabeceraTexto), ' letras ):'],'s');
+            texto = input(['Introduce mensaje a cifrar, mÃ¡ximo (', num2str(maxTextSize - BytesTotalesCabeceraTexto), ' letras ):'],'s');
             while size(texto,2) > maxTextSize
                 disp('Mensaje demasiado largo');
-                texto = input(['Introduce mensaje a cifrar, máximo (', num2str(maxTextSize-BytesTotalesCabeceraTexto), ' letras ):'],'s');
+                texto = input(['Introduce mensaje a cifrar, mÃ¡ximo (', num2str(maxTextSize-BytesTotalesCabeceraTexto), ' letras ):'],'s');
             end
             
             %Prepara el mensaje a cifrar
@@ -85,10 +85,10 @@ while opcionMenu ~= 0
             %mensaje.
             Cabecera= ['0',longitudBin];
         else
-            %% Introducción de imagen.
+            %% IntroducciÃ³n de imagen.
             continuar = false;
             disp('Introduce la imagen a ser cifrada');
-            disp(['Tamaño máximo ', num2str(2^BitsAltitud),'*',num2str(2^BitsLongitud)]);
+            disp(['TamaÃ±o mÃ¡ximo ', num2str(2^BitsAltitud),'*',num2str(2^BitsLongitud)]);
             while  ~continuar;
                 %obtiene la ruta mediante gestor de windows.
                 [fichero,ruta]=uigetfile({'*.jpg;*.bmp;*.png'},'Selecciona una imagen a cargar');
@@ -127,13 +127,13 @@ while opcionMenu ~= 0
         
         
         
-        %% Introducción del mensaje preparado en la imagen
+        %% IntroducciÃ³n del mensaje preparado en la imagen
         %En el bit 1 el tipo de dato
         % 0 para texto, 1 para imagen
         
         %Si es imagen
-        %En los bits 2-12 se guarda el tamaño X
-        %En los bits 13-23 se guarda el tamaño Y
+        %En los bits 2-12 se guarda el tamaÃ±o X
+        %En los bits 13-23 se guarda el tamaÃ±o Y
         %En el bit 24 se grayscale o rgb.
         
         %Si es texto
@@ -143,7 +143,7 @@ while opcionMenu ~= 0
         
         disp('Introduciendo mensaje en stream');
         ABinRellena = ABinVaciada;
-        %Rellena el bit nº8 con el mensaje.
+        %Rellena el bit nÂº8 con el mensaje.
         ABinRellena(1:size(mensajeConCabecera,2),8) = mensajeConCabecera;
         
         disp('Convirtiendo en imagen con mensaje cifrado');
@@ -154,17 +154,17 @@ while opcionMenu ~= 0
         imwrite(ARellena,'ImagenConMensaje.bmp');
         
         
-        %% Muestra imágen original, con el último bit vacío y con el cifrado.
+        %% Muestra imÃ¡gen original, con el Ãºltimo bit vacÃ­o y con el cifrado.
         figure;
         subplot(1,3,1);
         imshow(A);
         title('Imagen original');
         subplot(1,3,2)
         imshow(AVaciada);
-        title('Imagen con último bit vacío');
+        title('Imagen con Ãºltimo bit vacÃ­o');
         subplot(1,3,3)
         imshow(ARellena);
-        title('Imagen con mensaje en el último bit');
+        title('Imagen con mensaje en el Ãºltimo bit');
         
         %Limpiamos las variables creadas para que no interfieran en otras
         %ejecuciones.
@@ -227,8 +227,8 @@ while opcionMenu ~= 0
             clear MensajeTextoBin
         else
             %% Descodifica Imagen
-            %En los bits 2-12 se guarda el tamaño X
-            %En los bits 13-23 se guarda el tamaño Y
+            %En los bits 2-12 se guarda el tamaÃ±o X
+            %En los bits 13-23 se guarda el tamaÃ±o Y
             %En el bit 24 se guarda grayscale o rgb.
             
             % Leemos la cabecera
